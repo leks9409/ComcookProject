4cb913dd46108a814068d4adbfb32316d9a4d9fe
Comcook <lesk9409@naver.com>
2019-03-22 01:39:09 +0900

    로그인, 회원가입 추가.

--- src/main/java/com/cookstudy/controller/HomeController.java
+++ src/main/java/com/cookstudy/controller/HomeController.java
@@ -1,15 +1,22 @@
 package com.cookstudy.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.cookstudy.domain.Member;
+import com.cookstudy.persistence.Member_dao;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
+
+//import com.fasterxml.jackson.databind.DeserializationFeature;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Handles requests for the application home page.
@@ -17,23 +24,58 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	@Inject
+	private Member_dao m_dao;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	public String login() {
+		return "login";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "Login", method = RequestMethod.POST)
+	public int login(Member m, HttpSession session) {
+		return m_dao.Login(m, session);
+	}
+	
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public String register() {
+		return "register";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public int register(Member m) {
+		int result = m_dao.Register(m);
+		return result;
+	}
+	
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String index() {
 		return "home";
 	}
+	
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logout(HttpSession session) {
+		session.removeAttribute("m");
+		return "redirect:/";
+	}
+
+//	@ControllerAdvice
+//	public class JsonpAdviceController extends AbstractJsonpResponseBodyAdvice {
+//		public JsonpAdviceController() {
+//			super("callback");
+//		}
+//	}
+//
+//	@Bean
+//	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+//		MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+//		ObjectMapper objectMapper = new ObjectMapper();
+//		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+//		jsonConverter.setObjectMapper(objectMapper);
+//		return jsonConverter;
+//	}
 	
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,13 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
-<html>
-<head>
-	<title>Home</title>
-</head>
-<body>
-<h1>
-	Hello world!  
-</h1>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
-<P>  The time on the server is ${serverTime}. </P>
-</body>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>인덱스 데모</title>
+	</head>
+	<body>
+		${sessionScope.m.m_id}님 안녕하세요. <br>
+		<a href = "/logout">로그아웃</a>
+	</body>
 </html>
